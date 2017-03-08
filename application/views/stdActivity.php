37ca2e97a5bc28b14ddfeabfb3d6afa7072f7d5e
FBK\FOOKY <visitsak_99@hotmail.com>
2017-03-08 15:29:43 +0700

    no message

---
+++ application/views/stdActivity.php
@@ -0,0 +1,485 @@
+﻿
+
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+   
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <meta name="author" content="">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>Software Engineering</title>
+
+    <!--<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600' rel='stylesheet' type='text/css'>-->
+    <link href="<?php echo base_url(''); ?>assets/css/toolkit.css" rel="stylesheet">
+    
+    <link href="<?php echo base_url(''); ?>assets/css/application.css" rel="stylesheet">
+    
+
+    <link rel="stylesheet" href="<?php echo base_url(''); ?>assets/css/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url(''); ?>assets/fonts/th_k2d_july8.css" />
+
+   <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+ 
+    
+
+    <style>
+      /* note: this is a hack for ios iframe for bootstrap themes shopify page */
+      /* this chunk of css is not part of the toolkit :) */
+      body {
+        width: 1px;
+        min-width: 100%;
+        *width: 100%;
+        font-family: 'THK2DJuly8', sans-serif;
+        
+       font-weight: bold;
+      }
+    </style>
+
+  </head>
+
+
+<body class="bpj">
+
+  <div>
+    <ol class="breadcrumb breadcrumb-modi" style="background-color: transparent; margin: 25px 0 0 60px;">
+      <li><a href="#">Home</a></li>
+      <li class="active">ประวัติกิจกรรมของนักศึกษา</li>
+    </ol>
+
+  </div>
+
+  <div>
+
+  <!--<div class="bpw" id="app-growl"></div>-->
+
+<nav class="ck rj aeq ro vq app-navbar">
+  <button
+    class="re rh aye"
+    type="button"
+    data-toggle="collapse"
+    data-target="#navbarResponsive"
+    aria-controls="navbarResponsive"
+    aria-expanded="false"
+    aria-label="Toggle navigation">
+    <span class="rf"></span>
+  </button>
+
+  <a class="e" href="index.html">
+    <!--<img src="assets/img/brand-white.png" alt="brand">-->
+    LOGO
+  </a>
+
+  <div class="collapse f" id="navbarResponsive">
+    <ul class="navbar-nav ain">
+      <li class="qx active">
+        <a class="qv" href="schoolActivity.html">Home <span class="aet">(current)</span></a>
+      </li>
+      <li class="qx">
+        <a class="qv" href="schoolActivity.html">กิจกรรมหลักสูตร</a>
+      </li>
+      <li class="qx">
+        <a class="qv" href="#">ประวัติกิจกรรมของนักศึกษา</a>
+      </li>
+    
+
+     <!-- <li class="qx aye">
+        <a class="qv" href="notifications/index.html">Notifications</a>
+      </li>
+      <li class="qx aye">
+        <a class="qv" data-action="growl">Growl</a>
+      </li>
+     --> 
+      <li class="qx aye">
+        <a class="qv" href="login/index.html">Logout</a>
+      </li>
+
+    </ul>
+
+    <form class="pf aec ayd">
+      <input class="form-control" type="text" data-action="grow" placeholder="ค้นข้อมูลนักศึกษา">
+    </form>
+
+    <ul id="#js-popoverContent" class="nav navbar-nav aec afh ayd">
+      <li class="qx">
+        <a class="g qv" href="notifications/index.html">
+          <span class="h bbg"></span>
+        </a>
+      </li>
+      <li class="qx afx">
+        <button class="cg bqw bqx bpr" data-toggle="popover">
+          <img class="wg" src="<?php echo base_url(''); ?>assets/img/581222222.png">
+        </button>
+      </li>
+    </ul>
+
+    <ul class="nav navbar-nav aya" id="js-popoverContent">
+      <li class="qx"><a class="qv" href="#" data-action="growl">Growl</a></li>
+      <li class="qx"><a class="qv" href="login/index.html">Logout</a></li>
+    </ul>
+  </div>
+</nav>
+
+
+
+<div class="by ahy">
+  <div class="dq">
+    <div class="fh">
+      <div class="rp bqr agk">
+        <div class="rv" style="background-image: url(<?php echo base_url(''); ?>assets/img/blackblue8.jpg);"></div>
+        <div class="rq awx">
+          <a href="profile/index.html">
+            <img
+              class="bqs"
+              src="<?php echo base_url(''); ?>assets/img/581222222.png">
+          </a>
+
+          <h5 class="rr" style="font-weight: bold">
+            <a class="bpi" href="profile/index.html">น.ส ศิริภิญโญ จันทมุณี</a>
+          </h5>
+
+          <p class="agk"><hr></p>
+    
+          <ul class="bqt">
+            <li class="bqu">
+              <a href="#userModal" class="bpi" data-toggle="modal">
+                ชั้นปี
+                <h6 class="afl">1</h6>
+              </a>
+            </li>
+
+            <li class="bqu">
+              <a href="#userModal" class="bpi" data-toggle="modal">
+                รหัส
+                <h6 class="afl">581222222</h6>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+   
+      <div class="rp brb brc agk">
+        <div class="rq">
+          <h6 class="agd">About <small><a href="#">Edit</a></small></h6>
+          <!--<ul class="dc ayo">-->
+          <div >
+            <i class="fa fa-star"><span class="span-left">3.50</span></i>
+            <i class="fa fa-home"><span class="span-left">สงขลา</span></i>           
+            <i class="fa fa-user"><span class="span-left">ผศ.ดร.ฐิมาพร เพชรแก้ว</span></i>
+          </div>
+
+
+           <!-- <li><span class="axr h bok agc"></span>Became friends with <a href="#">Obama</a>
+            <li><span class="axr h bgi agc"></span>Worked at <a href="#">Github</a>
+            <li><span class="axr h bgy agc"></span>Lives in <a href="#">San Francisco, CA</a>
+            <li><span class="axr h bih agc"></span>From <a href="#">Seattle, WA</a>
+            -->
+          <!--</ul>-->
+        </div>
+      </div>
+
+      <div class="rp brb brc">
+        <div class="rq">
+          <h6 class="agd">Photos <small>· <a href="#">Edit</a></small></h6>
+          <div data-grid="images" data-target-height="150">
+            <div>
+              <img data-width="640" data-height="640" data-action="zoom" src="<?php echo base_url(''); ?>assets/img/instagram_5.jpg">
+            </div>
+
+            <div>
+              <img data-width="640" data-height="640" data-action="zoom" src="<?php echo base_url(''); ?>assets/img/instagram_6.jpg">
+            </div>
+
+            <div>
+              <img data-width="640" data-height="640" data-action="zoom" src="<?php echo base_url(''); ?>assets/img/instagram_7.jpg">
+            </div>
+
+            <div>
+              <img data-width="640" data-height="640" data-action="zoom" src="<?php echo base_url(''); ?>assets/img/instagram_8.jpg">
+            </div>
+
+            <div>
+              <img data-width="640" data-height="640" data-action="zoom" src="<?php echo base_url(''); ?>assets/img/instagram_9.jpg">
+            </div>
+
+            <div>
+              <img data-width="640" data-height="640" data-action="zoom" src="<?php echo base_url(''); ?>assets/img/instagram_10.jpg">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="fn">
+
+      <ul class="ca bqf bqg agk">
+
+        <li class="tu b agv">
+          <!--<div class="input-group">
+            <input type="text" class="form-control" placeholder="ค้นหากิจกรรม">
+            <div class="om">
+              <button type="button" class="cg pl">
+                <span class="glyphicon glyphicon-search"></span>
+              </button>
+            </div>
+          </div>-->
+          
+            <div style="margin: 0 0 0 15px">
+              <form class="navbar-form navbar-left" role="search">
+                  <div class="form-group">
+                      <input type="text" class="form-control" placeholder="ค้นหากิจกรรม">
+
+                  </div>
+              </form> 
+            </div>
+            <!--<div style="margin: 10px 0 10px 100px;">
+              <form class="pf aec ayd">
+               <input class="form-control" type="text" placeholder="ค้นข้อมูลนักศึกษา">
+             </form> 
+            </div>-->
+        </li>
+
+        
+        
+        <br>
+        
+        <li class="tu b ahx">
+          <!--<img
+            class="bqb wp yy agc"
+            src="assets/img/avatar-dhg.png">
+           --> 
+          <div class="tv">
+            <div class="bqk">
+              <small class="aec axr">2 กุมภาพันธ์ 2560</small>
+              <h6 style="font-weight: bold">กิจกรรมรับน้อง</h6>
+            </div>
+
+            <p>
+              นักศึกษาชั้นปีที่ 2 จัดงานรับน้อง
+            </p>
+
+            <!--<div class="bqh" data-grid="images">
+              <div style="display: none">
+                <img data-action="zoom" data-width="1050" data-height="700" src="assets/img/unsplash_1.jpg">
+              </div>
+
+              <div style="display: none">
+                <img data-action="zoom" data-width="640" data-height="640" src="assets/img/instagram_1.jpg">
+              </div>
+
+              <div style="display: none">
+                <img data-action="zoom" data-width="640" data-height="640" src="assets/img/instagram_13.jpg">
+              </div>
+
+              <div style="display: none">
+                <img data-action="zoom" data-width="1048" data-height="700" src="assets/img/unsplash_2.jpg">
+              </div>
+            </div>
+            -->
+            <!--<ul class="bqf afw">
+              <li class="tu agd">
+                <img
+                  class="bqb wp yy agc"
+                  src="assets/img/avatar-fat.jpg">
+                <div class="tv">
+                  <strong>Jacon Thornton: </strong>
+                  Donec id elit non mi porta gravida at eget metus. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Donec ullamcorper nulla non metus auctor fringilla. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Sed posuere consectetur est at lobortis.
+                </div>
+              </li>
+              <li class="tu">
+                <img
+                  class="bqb wp yy agc"
+                  src="assets/img/avatar-mdo.png">
+                <div class="tv">
+                  <strong>Mark Otto: </strong>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.
+                </div>
+              </li>
+            </ul>
+            -->
+          </div>
+        </li>
+
+        <br>
+
+        <li class="tu b ahx">
+         
+          <div class="tv">
+            <div class="bqk">
+              <small class="aec axr">3 กุมภาพันธ์ 2560</small>
+              <h6 style="font-weight: bold">กิจกรรมพี่สอนน้อง</h6>
+            </div>
+
+            <p>
+              XXXXXXXXXX
+            </p>
+    
+          </div>
+        </li>
+
+        <br>
+        <li class="tu b ahx">
+         
+          <div class="tv">
+            <div class="bqk">
+              <small class="aec axr">10 กุมภาพันธ์ 2560</small>
+              <h6 style="font-weight: bold">อบรม Yii Framework</h6>
+            </div>
+
+            <p>
+              XXXXXXXXXX
+            </p>
+    
+          </div>
+        </li>
+
+         <br>
+        <li class="tu b ahx">
+         
+          <div class="tv">
+            <div class="bqk">
+              <small class="aec axr">10 กุมภาพันธ์ 2560</small>
+              <h6 style="font-weight: bold">อบรม PHP advanced</h6>
+            </div>
+
+            <p>
+              XXXXXXXXXX
+            </p>
+    
+          </div>
+        </li>
+
+      </ul>
+
+      <div style="text-align: center;">
+        <nav aria-label="Page navigation">
+          <ul class="pagination">
+            <li>
+              <a href="#" aria-label="Previous">
+                <span aria-hidden="true">&laquo;</span>
+              </a>
+            </li>
+            <li><a href="#">1</a></li>
+            <li><a href="#">2</a></li>
+            <li><a href="#">3</a></li>
+            <li><a href="#">4</a></li>
+            <li><a href="#">5</a></li>
+            <li>
+              <a href="#" aria-label="Next">
+                <span aria-hidden="true">&raquo;</span>
+              </a>
+            </li>
+          </ul>
+        </nav>
+       </div> 
+
+
+        
+
+    </div>
+    <!--<div class="fh">
+      <div class="alert to alert-dismissible ayf" role="alert">
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <a class="tl" href="profile/index.html">Visit your profile!</a> Check your self, you aren't looking well.
+      </div>
+
+      <div class="rp agk ayf">
+        <div class="rq">
+          <h6 class="agd">Sponsored</h6>
+          <div data-grid="images" data-target-height="150">
+            <img class="bqb" data-width="640" data-height="640" data-action="zoom" src="assets/img/instagram_2.jpg">
+          </div>
+          <p><strong>It might be time to visit Iceland.</strong> Iceland is so chill, and everything looks cool here. Also, we heard the people are pretty nice. What are you waiting for?</p>
+          <button class="cg pq pz">Buy a ticket</button>
+        </div>
+      </div>
+
+      <div class="rp agk ayf">
+        <div class="rq">
+        <h6 class="agd">Likes <small>· <a href="#">View All</a></small></h6>
+        <ul class="bqf bqg">
+          <li class="tu afw">
+            <img
+              class="bqb wp yy agc"
+              src="assets/img/avatar-fat.jpg">
+            <div class="tv">
+              <strong>Jacob Thornton</strong> @fat
+              <div class="bqj">
+                <button class="cg pq pz">
+                  <span class="h azq"></span> Follow</button>
+              </div>
+            </div>
+          </li>
+           <li class="tu">
+            <a class="brd" href="#">
+              <img
+                class="bqb wp yy agc"
+                src="assets/img/avatar-mdo.png">
+            </a>
+            <div class="tv">
+              <strong>Mark Otto</strong> @mdo
+              <div class="bqj">
+                <button class="cg pq pz">
+                  <span class="h azq"></span> Follow</button></button>
+              </div>
+            </div>
+          </li>
+        </ul>
+        </div>
+        <div class="rw">
+          Dave really likes these nerds, no one knows why though.
+        </div>
+      </div>
+
+      <div class="rp bqv">
+        <div class="rq">
+          © 2015 Bootstrap
+          <a href="#">About</a>
+          <a href="#">Help</a>
+          <a href="#">Terms</a>
+          <a href="#">Privacy</a>
+          <a href="#">Cookies</a>
+          <a href="#">Ads </a>
+          <a href="#">Info</a>
+          <a href="#">Brand</a>
+          <a href="#">Blog</a>
+          <a href="#">Status</a>
+          <a href="#">Apps</a>
+          <a href="#">Jobs</a>
+          <a href="#">Advertise</a>
+        </div>
+      </div>
+    </div>-->
+  </div>
+</div>
+
+
+    <script src="<?php echo base_url(''); ?>assets/js/jquery.min.js"></script>    
+    <script src="<?php echo base_url(''); ?>assets/js/tether.min.js"></script>
+    <script src="<?php echo base_url(''); ?>assets/js/chart.js"></script>
+    <script src="<?php echo base_url(''); ?>assets/js/toolkit.js"></script>
+    <script src="<?php echo base_url(''); ?>assets/js/application.js"></script>
+    <script>
+      // execute/clear BS loaders for docs
+      $(function(){
+        if (window.BS&&window.BS.loader&&window.BS.loader.length) {
+          while(BS.loader.length){(BS.loader.pop())()}
+        }
+      })
+    </script>
+
+    </div>
+  </body>
+</html>
+
